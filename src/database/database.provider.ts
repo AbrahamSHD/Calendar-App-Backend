2a8031a1aff7057badc0f58514a2bf68ca60d934
Abraham HC <hernandez.c.abraham.00@gmail.com>
2024-10-01 21:59:34 -0600

    Database Module created

---
+++ src/database/database.provider.ts
@@ -0,0 +1,24 @@
+import { Logger, Provider } from '@nestjs/common';
+import * as mongoose from 'mongoose';
+import { envs } from 'src/common/config/envs';
+
+const logger = new Logger('DatabaseProvider');
+const url = envs.mongoDbUrl;
+
+export const databaseProvider: Provider[] = [
+  {
+    provide: 'DATABASE_CONNECTION',
+    useFactory: async (): Promise<typeof mongoose> => {
+      try {
+        logger.log('Database connected successfully');
+        const connection = await mongoose.connect(url);
+        return connection;
+      } catch (error) {
+        logger.error('Database connection failed', error.message);
+        throw new Error(
+          'Failed to connect to the database. Please check the connection.',
+        );
+      }
+    },
+  },
+];
